6d520ffb402c3a11e6f201d2dab875901442b3cf
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-11 23:57:39 +0200

    get location

--- boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
+++ boogle-maps/src/main/java/com/udacity/boogle/maps/MapsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/maps")
+@RequestMapping("/map")
 public class MapsController {
 
     @GetMapping
