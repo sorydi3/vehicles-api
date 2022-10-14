fe100f59b092fbb42e63e2eea9b2b67697fe2a51
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-14 14:48:50 +0200

    added parent pom files for submodules

--- serviceregistryeureka/src/test/java/com/example/serviceregistryeureka/ServiceregistryeurekaApplicationTests.java
+++ serviceregistryeureka/src/test/java/com/example/serviceregistryeureka/ServiceregistryeurekaApplicationTests.java
@@ -1,12 +1,11 @@
 package com.example.serviceregistryeureka;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiceregistryeurekaApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
