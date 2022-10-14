8ec85b356a786830de08b9c51ec4e4cf729dbb13
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-14 18:56:27 +0200

    added swagger to pricing services

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -4,12 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * DONE: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableSwagger2
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
