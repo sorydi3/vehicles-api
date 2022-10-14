64b1b7283112e26c7c30c5dd5d2c670cb69b8e80
ibradi3 <ibrahimasory22@hotmail.com>
2022-10-14 18:22:54 +0200

    swagger added to carService

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -6,12 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
- * TODO: Convert the application from a REST API to a microservice.
+ * DONE: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
 @EnableEurekaClient
-
-
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
